40c9a180e6fe243d4e88eabc8304be5cb63672d1
Luiz Henrique <106573095+luiz-henrique-castro@users.noreply.github.com>
2022-09-11 05:13:05 -0300

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,12 @@
+document.getElementById("hiden")
+.addEventListener("click", hiden)
+function hiden(){
+    document.getElementById("content").style.display="none"
+
+}
+document.getElementById("show")
+.addEventListener("click", show)
+function show(){
+    document.getElementById("content").style.display="inline"
+
+}
